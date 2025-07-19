9539ec7c42fbe51200058acc6433b39838ef8136
onion-chen <chenshicong666@qq.com>
2025-07-19 11:29:17 +0800

    添加评论

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,13 @@
 import DefaultTheme from 'vitepress/theme';
 import CppHome from './components/CppHome.vue';
+import Utterances from './components/Utterances.vue'
 import './styles/custom-containers.css'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     // 注册全局组件
-    app.component('CppHome', CppHome)
+    app.component('CppHome', CppHome);
+    app.component('Utterances', Utterances);
   }
 }
--- docs/cpp/index.md
+++ docs/cpp/index.md
@@ -1,1 +1,2 @@
 <CppHome/>
+<Utterances/>
